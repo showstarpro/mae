fb0209a08753a46e9fadec114e3cec604c45fb4a
loyingue <113994617+loyingue@users.noreply.github.com>
2025-04-19 13:55:13 +0800

    Update ade20k.py

--- mmsegmentation/configs/_base_/datasets/ade20k.py
+++ mmsegmentation/configs/_base_/datasets/ade20k.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'ADE20KDataset'
-data_root = '/mnt/datasets/ade20k/0-1-0/ADEChallengeData2016'
+data_root = '/lpai/dataset/ade20k/0-1-0/ADEChallengeData2016'
 crop_size = (512, 512)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
